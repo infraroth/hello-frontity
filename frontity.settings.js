036bb34219d84772d6bbfe2afb09ec7514d0a4d3
Julien Roth <julien@infraroth.com>
2022-02-19 19:14:23 +0100

    Initial commit

---
+++ frontity.settings.js
@@ -0,0 +1,27 @@
+const settings = {
+  "name": "hello-frontity",
+  "packages": [
+    {
+      "name": "my-first-theme",
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "https://test.frontity.org",
+          "postTypes": [
+            {
+              type: "destinations",
+              endpoint: "destinations",
+              archive: "/destinations"
+            }
+          ]
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
